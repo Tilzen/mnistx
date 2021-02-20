0d6e83d0ef75cafbe1f6e690d7195ab760f72566
Tilzen <lucas.nascimentoR@hotmail.com>
2021-02-20 11:36:09 -0300

    project: Adiciona esqueleto inicial do projeto.

---
+++ lib/mnistx.ex
@@ -0,0 +1,18 @@
+defmodule Mnistx do
+  @moduledoc """
+  Documentation for `Mnistx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mnistx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mnistx_test.exs
@@ -0,0 +1,8 @@
+defmodule MnistxTest do
+  use ExUnit.Case
+  doctest Mnistx
+
+  test "greets the world" do
+    assert Mnistx.hello() == :world
+  end
+end
